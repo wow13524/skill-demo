39d87874979f4e22f4917f44a57e79f2153e3add
wow13524 <50389483+wow13524@users.noreply.github.com>
2022-05-01 20:18:56 -0700

    Made testMin not static

--- MinTest.java
+++ MinTest.java
@@ -2,7 +2,7 @@ import static org.junit.Assert.*;
 import org.junit.*;
 public class MinTest {
     @Test
-    public static void testMin(){
+    public void testMin(){
         assertEquals(2,Min.min(2,4));
     }
 }
