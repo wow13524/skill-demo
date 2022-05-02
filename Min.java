3a3f8cce67cd1100053de4d2c3adcf10bc6c4ac8
wow13524 <50389483+wow13524@users.noreply.github.com>
2022-05-01 20:17:11 -0700

    Created Min class and corresponding unit test that SHOULD fail

---
+++ Min.java
@@ -0,0 +1,5 @@
+public class Min{
+    public static int min(int a, int b){
+        return b;
+    }
+}
---
+++ MinTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+public class MinTest {
+    @Test
+    public static void testMin(){
+        assertEquals(2,Min.min(2,4));
+    }
+}
